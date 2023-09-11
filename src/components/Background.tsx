3632dbafbea2878877bcb4ddc71c5e79701c8537
sebyet <sebastien.payet9@gmail.com>
2023-09-11 21:50:29 +0200

    update

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,11 +2,13 @@ import React, { type ReactNode } from 'react';
 
 interface BackgroundProps {
   children: ReactNode;
+  white?: boolean,
 }
 
-const Background: React.FC<BackgroundProps> = ({ children }) => {
+const Background: React.FC<BackgroundProps> = ({ children, white }) => {
+  const BackgroundColorClass = white ? `bg-white}` : 'bg-slate-50';
   return (
-    <div className={`bg-white`}>
+    <div className={`flex flex-col justify-center items-center w-full h-96 gap-5 ${BackgroundColorClass}`}>
       {children}
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from './Base/Button';
 
 const Navbar = () => {
     return (
-      <div className={`pt-5 flex justify-between items-center w-full`}>
+      <div className="sticky top-0 pt-5 flex justify-between items-center w-full">
         <H1>Test</H1>
         <Button>Contact</Button>
       </div>
